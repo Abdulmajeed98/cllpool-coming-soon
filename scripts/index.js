0333ba5c326c2c5e2b546d02526e20e37e141df3
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-12-07 23:55:58 +0300

    handled toggle theme and added final responsivness queries

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const countDownDate = 1643706000000 / 1000;
     // console.log(new Date().getTime());
-    const flipdown = new FlipDown(countDownDate, {
-        theme: 'light',
-    });
+    const flipdown = new FlipDown(countDownDate);
     flipdown.start();
     flipdown.ifEnded(console.log('countdown finished'));
 
+
+    // toggle flipdown theme
+    const toggleTheme = setInterval(() => {
+        document.querySelector('#flipdown').classList.toggle('flipdown__theme-dark');
+        document.querySelector('#flipdown').classList.toggle('flipdown__theme-light');
+    }, 1000);
 })
