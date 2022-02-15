f5de50d2d6c8577807792c13423315dcb27a182e
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2022-02-15 15:24:39 +0300

    remove console.log() statements

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const countDownDate = 1646168399000 / 1000;
     const date = new Date(countDownDate * 1000);
     const options = { year: 'numeric', month: 'long', day: 'numeric' }
-    console.log(date.toLocaleDateString('en-US', options));
     const arrayChildren = Array.from(launchText.children);
     arrayChildren[arrayChildren.length - 1].innerText += " " + date.toLocaleDateString('en-US', options);
-    // console.log(new Date().getTime());
     const flipdown = new FlipDown(countDownDate, {
         theme: 'light',
     });
